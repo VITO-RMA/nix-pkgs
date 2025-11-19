cfe940cca195475252c5acdfeec4d55f854ed328
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-11-19 23:41:08 +0100

    Switch libxlsxwriter to mkDerivation and update to 1.2.3

--- pkgs/libxlsxwriter.nix
+++ pkgs/libxlsxwriter.nix
@@ -1,19 +1,53 @@
 {
   lib,
   stdenv,
-  libxlsxwriter,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
   minizip,
   zlib,
+  openssl ? null,
   static ? stdenv.hostPlatform.isStatic,
 }:
 
-(libxlsxwriter.override { }).overrideAttrs (old: {
-  doCheck = false;
+stdenv.mkDerivation rec {
+  pname = "libxlsxwriter";
+  version = "1.2.3";
+
+  src = fetchFromGitHub {
+    owner = "jmcnamara";
+    repo = "libxlsxwriter";
+    tag = "v${version}";
+    hash = "sha256-1FUJLsnx0ZNTT66sK7/gbZVo6Se85nbYvtEyoxeOHTI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
 
   buildInputs = [
     minizip
     zlib
+    openssl
   ];
 
-  cmakeFlags = old.cmakeFlags or [ ] ++ [ (lib.cmakeBool "BUILD_SHARED_LIBS" (!static)) ];
-})
+  cmakeFlags = [
+    "-DUSE_SYSTEM_MINIZIP=1"
+    "-DWINDOWSSTORE=OFF"
+    "-DUSE_DTOA_LIBRARY=OFF"
+    "-DUSE_MEM_FILE=ON"
+    "-DUSE_OPENSSL_MD5=ON"
+  ]
+  ++ [ (lib.cmakeBool "USE_OPENSSL_MD5" (openssl != null)) ]
+  ++ [ (lib.cmakeBool "BUILD_SHARED_LIBS" (!static)) ];
+
+  meta = with lib; {
+    description = "C library for creating Excel XLSX files";
+    homepage = "https://libxlsxwriter.github.io/";
+    changelog = "https://github.com/jmcnamara/libxlsxwriter/blob/${src.rev}/Changes.txt";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ dotlambda ];
+    platforms = platforms.unix;
+  };
+}
