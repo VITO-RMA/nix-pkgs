19768924a05aa1682008d8eef67fdcab41d9d518
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-11-22 00:14:13 +0100

    Fix mingw compilation for cryptopp package

--- pkgs/cryptopp.nix
+++ pkgs/cryptopp.nix
@@ -11,5 +11,9 @@
   (old: {
     pname = mkPackageName old.pname static stdenv;
     doCheck = false;
-    mingwSupport = false;
+
+    # Fix mingw compilation
+    NIX_CFLAGS_COMPILE =
+      (old.NIX_CFLAGS_COMPILE or "")
+      + (if stdenv.hostPlatform.isMinGW then " -D_WIN32_WINNT=0x0600" else "");
   })
