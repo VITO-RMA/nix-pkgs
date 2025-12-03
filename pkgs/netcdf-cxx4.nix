60e1fecf4afbd156cd98c049e94446a55de9534f
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-12-03 09:59:50 +0100

    Export CMake targets for netcdf-cxx4 package

--- pkgs/netcdf-cxx4.nix
+++ pkgs/netcdf-cxx4.nix
@@ -17,11 +17,13 @@
 
     patches = [
       ./patches/netcdf-cxx4-hdf5-dep.patch
+      ./patches/netcdf-cxx4-export-cmake-targets.patch
     ];
 
     postPatch = ''
       substituteInPlace CMakeLists.txt \
         --replace "cmake_minimum_required(VERSION 2.8.12)" "cmake_minimum_required(VERSION 3.21)"
+      cp ${./patches/netCDFCxxConfig.cmake.in} netCDFCxxConfig.cmake.in
     '';
 
     buildInputs = [
@@ -31,6 +33,7 @@
 
     cmakeFlags = old.cmakeFlags or [ ] ++ [
       "-DNCXX_ENABLE_TESTS=OFF"
+      "-DCMAKE_INSTALL_CMAKECONFIGDIR=lib/cmake/netCDFCxx"
       (lib.cmakeBool "NC_HAS_DEF_VAR_FILTER" (!static)) # do not build the plugins in static builds
       (lib.cmakeBool "HDF5_USE_STATIC_LIBRARIES" static)
       (lib.cmakeBool "BUILD_SHARED_LIBS" (!static))
