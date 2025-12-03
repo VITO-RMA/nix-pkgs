3062ca2c1ec7d0edb0d1748a8ec4bebc7b41e96d
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-12-03 10:17:22 +0100

    Use cmake to build shapelib

--- pkgs/shapelib.nix
+++ pkgs/shapelib.nix
@@ -1,19 +1,38 @@
+# upstream package uses autotools so there is no cmake module
 {
   lib,
   stdenv,
-  shapelib,
+  fetchurl,
+  cmake,
   static ? stdenv.hostPlatform.isStatic,
   mkPackageName,
 }:
 
-shapelib.overrideAttrs (old: {
-  pname = mkPackageName old.pname static stdenv;
-  doCheck = false;
+stdenv.mkDerivation rec {
+  pname = mkPackageName "shapelib" static stdenv;
+  version = "1.6.2";
 
-  configureFlags =
-    (old.configureFlags or [ ])
-    ++ lib.optionals static [
-      "--enable-static"
-      "--disable-shared"
-    ];
-})
+  src = fetchurl {
+    url = "https://download.osgeo.org/shapelib/shapelib-${version}.tar.gz";
+    hash = "sha256-S3SjbO2U6ae+pAEVfmZK3cxb4lHn33+I1GdDYdoBLCE=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  cmakeFlags = [
+    "-DBUILD_SHAPELIB_CONTRIB=OFF"
+    "-DBUILD_APPS=OFF"
+    "-DBUILD_TESTING=OFF"
+    "-DUSE_RPATH=OFF"
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!static))
+  ];
+
+  meta = with lib; {
+    description = "C Library for reading, writing and updating ESRI Shapefiles";
+    homepage = "http://shapelib.maptools.org/";
+    license = licenses.gpl2;
+    changelog = "http://shapelib.maptools.org/release.html";
+  };
+}
