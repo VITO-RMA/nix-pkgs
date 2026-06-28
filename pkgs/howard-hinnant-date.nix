3cf29575b1f61ac5b49d15e2691da1c7a0982154
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-28 22:42:13 +0200

    Patch date CMake condition for MinGW builds

--- pkgs/howard-hinnant-date.nix
+++ pkgs/howard-hinnant-date.nix
@@ -12,6 +12,13 @@
     pname = mkPackageName old.pname static stdenv;
     doCheck = false;
 
+    postPatch = (old.postPatch or "") + ''
+      substituteInPlace CMakeLists.txt \
+        --replace-fail \
+          'if( WIN32 AND NOT CYGWIN)' \
+          'if( WIN32 AND NOT MINGW)'
+    '';
+
     cmakeFlags = [
       "-DBUILD_TZ_LIB=ON"
       "-DUSE_SYSTEM_TZ_DB=ON"
